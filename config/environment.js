808aa9b23436b6da1d7340e5950dbb2363047e14
manohar199814 <manoharmanu2614@gmail.com>
2022-11-27 17:43:10 +0530

    fixed bugs

--- config/environment.js
+++ config/environment.js
@@ -14,24 +14,24 @@ const accessLogStream = rfs.createStream('access.log', {
 
 
 const development = {
-    name: 'development',
-    asset_path: '/assets',
-    session_cookie_key: 'blahsomething',
-    db: 'codeial_development',
-    smtp: {
-        service: 'gmail',
-        host: 'smtp.gmail.com',
+    name:'development',
+    asset_path:'assets',
+    session_cookie_key:'blahsomething',
+    db:'codeial_development',
+    smtp:{
+        service:'gmail',
+        host: "smtp.gmail.com",
         port: 587,
-        secure: false,
+        secure: false, // true for 465, false for other ports
         auth: {
-            user: 'manoharmanu2614',
-            pass: 'ngznhctlojrnobug'
-        }
+          user: 'manoharmanu2614', // gmail user name
+          pass: 'ngznhctlojrnobug', // gmail password
+        },
     },
-    google_client_id: "313233209747-dnqmail3j800a2jvsuckqhohodhs7i63.apps.googleusercontent.com",
-    google_client_secret: "0FXb5EBWa4xRfJ8jR-1HKMd2",
-    google_call_back_url: "http://localhost:8000/users/auth/google/callback",
-    jwt_secret: 'codeial',
+    google_client_id:'27346747973-f4bfdga5t0094l7fmmcltrj40ijv0n4q.apps.googleusercontent.com',
+    google_client_secret: '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',
+    google_callback_url: "http://localhost:8000/user/auth/google/callback",
+    jwt_secret:'codeial',
     morgan: {
         mode: 'dev',
         options: {stream: accessLogStream}
